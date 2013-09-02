3d2c99b02eb064ae7c14bb2c547a61eea022f17c
Kevin Whitley <kevin3503@gmail.com>
2013-09-02 16:24:34 -0500

    Adding readme

--- README.md
+++ README.md
@@ -1,4 +1,82 @@
 treeize
 =======
 
-Converts row data (in JSON/associative array format) to tree structure based on column naming conventions.
+_v0.0.3_
+
+Converts row data (in JSON/associative array format) to object/tree structure based on column naming conventions.
+
+## Installation
+
+`npm install --save treeize`
+
+## Usage
+
+Treeize currently has a single function, `grow`, that takes your results/rows of flat associative data and returns a full object graph;
+
+- The column/attribute order is not important.  All attributes are sorted by depth before mapping.
+- To imply a collection, use a plural name (e.g. "subjects" instead of "subject").  Otherwise, use a singular name.
+- Use a `+` to seperate path nodes (e.g. "toys+name" implies a "toys" collection on a root level item.  Each toy item would have [at least] a "name" attribute)
+
+```
+var treeize = require('treeize');
+
+var flatData = [
+  {
+    "name":             "Mittens",
+    "age":              12,
+    "toys+name":        "mouse",
+    "toys+owner+name":  "Mittens"
+  },
+  {
+    "name":             "Mittens",
+    "age":              12,
+    "toys+name":        "yarn",
+    "toys+owner+name":  "Ms. Threadz"
+  },
+  {
+    "name":             "Tiger",
+    "age":              7,
+    "toys+name":        "a stick",
+    "toys+owner+name":  "Mother Nature"
+  }
+];
+
+var converted = treeize.grow(flatData);
+```
+
+### Output
+
+```
+[
+  {
+    "name": "Mittens",
+    "age": 12,
+    "toys": [
+      {
+        "name": "mouse",
+        "owner": {
+          "name": "Mittens"
+        }
+      },
+      {
+        "name": "yarn",
+        "owner": {
+          "name": "Ms. Threadz"
+        }
+      }
+    ]
+  },
+  {
+    "name": "Tiger",
+    "age": 7,
+    "toys": [
+      {
+        "name": "a stick",
+        "owner": {
+          "name": "Mother Nature"
+        }
+      }
+    ]
+  }
+]
+```
---
+++ bin/treeize.js
@@ -0,0 +1,90 @@
+var _                   = require('lodash');
+_.inflection            = require('inflection');
+_.mixin(_.inflection);
+
+function expand(results) {
+  if (!results || !results.length) { return results; }
+
+  var translated = [];
+
+  _.each(results, function(row, index) {
+    var translatedItem = {};
+    var paths = [];
+    var trails = {};
+
+    // set up paths for processing
+    _.each(row, function(value, fullPath) {
+      var splitPath = fullPath.split('+');
+
+      paths.push({
+        splitPath:  splitPath.slice(0, splitPath.length - 1),
+        fullPath:   splitPath.slice(0, splitPath.length - 1).join('+'),
+        parentPath: splitPath.slice(0, splitPath.length - 2).join('+'),
+        node:       splitPath.slice(splitPath.length - 2, splitPath.length - 1).join('+'),
+        attribute:  _.last(splitPath),
+        value:      value,
+        processed:  false
+      });
+    });
+
+    // sort paths to prepare for processing
+    paths.sort(function(a, b) {
+      if (a.splitPath.length !== b.splitPath.length) {
+        return a.splitPath.length < b.splitPath.length ? -1 : 1;
+      }
+
+      return a.parentPath < a.parentPath ? -1 : 1;
+    });
+
+    // proccess each unprocessed path in the row
+    var trail = translated;
+
+    while (_.findWhere(paths, { processed: false })) {
+      var target = _.findWhere(paths, { processed: false });
+
+      // get associated group
+      var group = _.where(paths, { parentPath: target.parentPath, node: target.node, processed: false });
+
+      // build blueprint for current group
+      var blueprint = {};
+      _.each(group, function(groupItem) {
+        blueprint[groupItem.attribute] = groupItem.value;
+        groupItem.processed = true;
+      });
+
+      trail = trails[target.parentPath];
+
+      // set up first node, everythign else should have parent path
+      if (!trail) {
+        trail = _.findWhere(translated, blueprint);
+        if (!trail) {
+          trail = blueprint;
+          translated.push(blueprint);
+        }
+        trails[target.parentPath] = trail;
+      }
+
+      // trail is now at parent node, standing by for current node injection
+      if (target.node) { // should skip root because '' resolves to false
+        var isCollection = target.node === _.pluralize(target.node);
+        var node = trail[target.node] = (trail[target.node] || (isCollection ? [blueprint] : blueprint));
+
+        if (isCollection) {
+          node = _.findWhere(trail[target.node], blueprint);
+          if (!node) {
+            node = blueprint;
+            trail[target.node].push(node);
+          }
+        }
+
+        trails[target.fullPath] = node;
+      }
+    }
+  });
+
+  return translated;
+}
+
+exports.grow = function(rows, config) {
+  return expand(rows);
+}
