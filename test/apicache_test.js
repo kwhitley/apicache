850a42d6d01fae67bb5d19fb4f8e05bb752e66b6
Kevin Whitley <kevin3503@gmail.com>
2016-10-22 02:05:59 -0500

    added status code white list and black listing

--- test/apicache_test.js
+++ test/apicache_test.js
@@ -1,172 +1,217 @@
-var chai = require('chai');
-var expect = chai.expect;
-var request = require('supertest');
+var chai = require('chai')
+var expect = chai.expect
+var request = require('supertest')
+var apicache = require('../lib/apicache')
+var a = apicache.clone()
+var b = apicache.clone()
+var c = apicache.clone()
 
 describe('.options(opt?) {GETTER/SETTER}', function() {
-  var apicache = require('../lib/apicache.js');
+  var apicache = require('../lib/apicache.js')
 
   it('is a function', function() {
-    expect(typeof apicache.options).to.equal('function');
-  });
-  
+    expect(typeof apicache.options).to.equal('function')
+  })
+
   describe('.options() {GETTER}', function() {
     it ('returns global options as object', function() {
-      expect(typeof apicache.options()).to.equal('object');
-    });
-  });
+      expect(typeof apicache.options()).to.equal('object')
+    })
+  })
 
   describe('.options(opt) {SETTER}', function() {
     it ('is chainable', function() {
-      expect(apicache.options({})).to.equal(apicache);
-    });
-     
+      expect(apicache.options({})).to.equal(apicache)
+    })
+
     it ('extends defaults', function() {
-      expect(apicache.options({ foo: 'bar' }).options().foo).to.equal('bar');
-    });
+      expect(apicache.options({ foo: 'bar' }).options().foo).to.equal('bar')
+    })
 
     it ('allows overrides of defaults', function() {
-      var newDuration = 11;
+      var newDuration = 11
 
-      expect(apicache.options()).to.have.property('defaultDuration');
-      expect(apicache.options({ defaultDuration: newDuration }).options().defaultDuration).to.equal(newDuration);
-    });
-  });
-});
+      expect(apicache.options()).to.have.property('defaultDuration')
+      expect(apicache.options({ defaultDuration: newDuration }).options().defaultDuration).to.equal(newDuration)
+    })
+  })
+})
 
 describe('.getDuration(stringOrNumber) {GETTER}', function() {
-  var apicache = require('../lib/apicache.js');
+  var apicache = require('../lib/apicache.js')
 
   it('is a function', function() {
-    expect(typeof apicache.getDuration).to.equal('function');
-  });
-  
+    expect(typeof apicache.getDuration).to.equal('function')
+  })
+
   it('returns value unchanged if numeric', function() {
-    expect(apicache.getDuration(77)).to.equal(77);
-  });
+    expect(apicache.getDuration(77)).to.equal(77)
+  })
 
   it('returns default duration when uncertain', function() {
-    apicache.options({ defaultDuration: 999 });
-    expect(apicache.getDuration(undefined)).to.equal(999);
-  });
+    apicache.options({ defaultDuration: 999 })
+    expect(apicache.getDuration(undefined)).to.equal(999)
+  })
 
   it('accepts singular or plural (e.g. "1 hour", "3 hours")', function() {
-    expect(apicache.getDuration('3 seconds')).to.equal(3000);
-    expect(apicache.getDuration('3 second')).to.equal(3000);
-  });
+    expect(apicache.getDuration('3 seconds')).to.equal(3000)
+    expect(apicache.getDuration('3 second')).to.equal(3000)
+  })
 
   it('accepts decimals (e.g. "1.5 hours")', function() {
-    expect(apicache.getDuration('1.5 seconds')).to.equal(1500);
-  });
+    expect(apicache.getDuration('1.5 seconds')).to.equal(1500)
+  })
 
   describe('unit support', function() {
+    it('numeric values as milliseconds', function() {
+      expect(apicache.getDuration(43)).to.equal(43)
+    })
+    it('milliseconds', function() {
+      expect(apicache.getDuration('3 ms')).to.equal(3)
+    })
     it('seconds', function() {
-      expect(apicache.getDuration('3 seconds')).to.equal(3000);
-    });
+      expect(apicache.getDuration('3 seconds')).to.equal(3000)
+    })
     it('minutes', function() {
-      expect(apicache.getDuration('4 minutes')).to.equal(1000 * 60 * 4);
-    });
+      expect(apicache.getDuration('4 minutes')).to.equal(1000 * 60 * 4)
+    })
     it('hours', function() {
-      expect(apicache.getDuration('2 hours')).to.equal(1000 * 60 * 60 * 2);
-    });
+      expect(apicache.getDuration('2 hours')).to.equal(1000 * 60 * 60 * 2)
+    })
     it('days', function() {
-      expect(apicache.getDuration('3 days')).to.equal(1000 * 60 * 60 * 24 * 3);
-    });
+      expect(apicache.getDuration('3 days')).to.equal(1000 * 60 * 60 * 24 * 3)
+    })
     it('weeks', function() {
-      expect(apicache.getDuration('5 weeks')).to.equal(1000 * 60 * 60 * 24 * 7 * 5);
-    });
+      expect(apicache.getDuration('5 weeks')).to.equal(1000 * 60 * 60 * 24 * 7 * 5)
+    })
     it('months', function() {
-      expect(apicache.getDuration('6 months')).to.equal(1000 * 60 * 60 * 24 * 30 * 6);
-    });
+      expect(apicache.getDuration('6 months')).to.equal(1000 * 60 * 60 * 24 * 30 * 6)
+    })
     it('years', function() {
-      expect(apicache.getDuration('2 years')).to.equal(1000 * 60 * 60 * 24 * 365 * 2);
-    });
-  });
-  
-});
+      expect(apicache.getDuration('2 years')).to.equal(1000 * 60 * 60 * 24 * 365 * 2)
+    })
+  })
+
+})
 
 describe('.getIndex() {GETTER}', function() {
-  var apicache = require('../lib/apicache.js');
+  var apicache = require('../lib/apicache.js')
 
   it('is a function', function() {
-    expect(typeof apicache.getIndex).to.equal('function');
-  });
-  
+    expect(typeof apicache.getIndex).to.equal('function')
+  })
+
   it('returns an object', function() {
-    expect(typeof apicache.getIndex()).to.equal('object');
-  });
-});
+    expect(typeof apicache.getIndex()).to.equal('object')
+  })
+})
 
 describe('.resetIndex() {SETTER}', function() {
-  var apicache = require('../lib/apicache.js');
+  var apicache = require('../lib/apicache.js')
 
   it('is a function', function() {
-    expect(typeof apicache.resetIndex).to.equal('function');
-  });
+    expect(typeof apicache.resetIndex).to.equal('function')
+  })
 
-});
+})
 
 describe('.clear(key?) {SETTER}', function() {
-  var apicache = require('../lib/apicache.js');
+  var apicache = require('../lib/apicache.js')
 
   it('is a function', function() {
-    expect(typeof apicache.clear).to.equal('function');
-  });
+    expect(typeof apicache.clear).to.equal('function')
+  })
 
-});
+})
 
 describe('.middleware {MIDDLEWARE}', function() {
-  var mockAPI = require('./mock_api')('1 second');
-  var apicache = require('../lib/apicache.js');
+  var apicache = require('../lib/apicache.js')
 
   it('is a function', function() {
-    expect(typeof apicache.middleware).to.equal('function');
-  });
+    expect(typeof apicache.middleware).to.equal('function')
+  })
 
   it('does not interfere with initial request', function(done) {
+    var mockAPI = require('./mock_api')('10 seconds')
+
     request(mockAPI)
       .get('/api/movies')
-      .expect('Content-Type', /json/)
-      .expect(200)
       .end(function(err, res) {
-        done();
-      });
-  });
+        // console.log('res', mockAPI.apicache.id, mockAPI.apicache.getIndex())
+        expect(mockAPI.requestsProcessed).to.equal(1)
+        done()
+      })
+  })
 
   it('caches a request', function(done) {
+    var mockAPI = require('./mock_api')('10 seconds')
+
     request(mockAPI)
       .get('/api/movies')
-      .expect('Content-Type', /json/)
-      .expect(200)
       .end(function(err, res) {
-        expect(mockAPI.requestsProcessed).to.equal(1);
-        done();
-      });
-  });
+        // console.log('res', mockAPI.apicache.id, mockAPI.apicache.getIndex())
+        expect(mockAPI.apicache.getIndex().all.length).to.equal(1)
+        expect(mockAPI.apicache.getIndex().all).to.include('/api/movies')
+        expect(mockAPI.requestsProcessed).to.equal(1)
+        done()
+      })
+  })
+
+  it('does not cache a request when status code found in status code exclusions', function(done) {
+    var mockAPI = require('./mock_api')('2 seconds', {
+      statusCodes: { exclude: [404] }
+    })
+
+    request(mockAPI)
+      .get('/api/missing')
+      .end(function(err, res) {
+        expect(res.statusCode).to.equal(404)
+        expect(mockAPI.apicache.getIndex().all.length).to.equal(0)
+        done()
+      })
+  })
+
+  it('does not cache a request when status code not found in status code inclusions', function(done) {
+    var mockAPI = require('./mock_api')('2 seconds', {
+      statusCodes: { include: [200] }
+    })
+
+    request(mockAPI)
+      .get('/api/missing')
+      .end(function(err, res) {
+        expect(res.statusCode).to.equal(404)
+        expect(mockAPI.apicache.getIndex().all.length).to.equal(0)
+        done()
+      })
+  })
 
   // it('skips cache when middlewareToggle param is falsey', function(done) {
-  //   var falseAPI = require('./mock_api')('1 second', false);
+  //   var falseAPI = require('./mock_api')('1 second', false)
   //   request(falseAPI)
   //     .get('/api/movies')
   //     .end(function(err, res) {
   //       request(falseAPI)
   //         .get('/api/movies')
   //         .end(function(err, res) {
-  //           expect(falseAPI.requestsProcessed).to.equal(1);
-  //           done();
+  //           expect(falseAPI.requestsProcessed).to.equal(1)
+  //           done()
   //         })
-  //     });
-  // });
-
-  it('injects a cache key', function() {
-    expect(apicache.getIndex().all).to.include('/api/movies');
-  });
+  //     })
+  // })
 
   it('removes a cache key after expiration', function(done) {
-    this.timeout(5000);
+    var mockAPI = require('./mock_api')(10)
+    request(mockAPI)
+      .get('/api/movies')
+      .end(function(err, res) {
+        expect(mockAPI.apicache.getIndex().all.length).to.equal(1)
+        expect(mockAPI.apicache.getIndex().all).to.include('/api/movies')
+      })
+
     setTimeout(function() {
-      expect(apicache.getIndex().all).to.have.length(0);
-      done();
-    }, 1001);
-  });
-  
-});
+      expect(mockAPI.apicache.getIndex().all).to.have.length(0)
+      done()
+    }, 15)
+  })
+
+})
