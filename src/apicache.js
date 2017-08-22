8fc0bad850d4936d189f39fd632ff88d682aaeac
Andre Di Genova <andre.digenova@tmx.com>
2017-08-22 10:58:58 -0400

    Add option to disable header caching.

--- src/apicache.js
+++ src/apicache.js
@@ -37,6 +37,7 @@ function ApiCache() {
     appendKey:          [],
     jsonp:              false,
     redisClient:        false,
+    cacheHeaders:       true,
     statusCodes: {
       include: [],
       exclude: [],
@@ -90,7 +91,7 @@ function ApiCache() {
   function createCacheObject(status, headers, data, encoding) {
     return {
       status: status,
-      headers: Object.assign({}, headers),
+      headers: Object.assign({}, globalOptions.cacheHeaders ? headers : {}),
       data: data,
       encoding: encoding
     }
@@ -184,7 +185,8 @@ function ApiCache() {
 
   function sendCachedResponse(response, cacheObject) {
     var headers = (typeof response.getHeaders === 'function') ? response.getHeaders() : response._headers;
-    Object.assign(headers, cacheObject.headers || {}, {
+    var cachedHeaders = globalOptions.cacheHeaders ? cacheObject.headers : {};
+    Object.assign(headers, cachedHeaders || {}, {
       'apicache-store': globalOptions.redisClient ? 'redis' : 'memory',
       'apicache-version': pkg.version
     })
